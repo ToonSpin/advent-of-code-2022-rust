6726ee9764c5b718b1e8c72ee6865a5f48ce3ef4
Toon Spin <toon.spin@gmail.com>
2022-12-11 13:15:25 +0100

    Day 11 part two

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,13 +1,13 @@
 use std::io;
 use std::io::prelude::*;
 
-use nom::IResult;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, line_ending};
-use nom::combinator::{map_res, value, map};
+use nom::combinator::{map, map_res, value};
 use nom::multi::separated_list1;
 use nom::sequence::{delimited, pair, tuple};
+use nom::IResult;
 
 type Item = u64;
 
@@ -54,7 +54,7 @@ struct Monkey {
     modulus: u64,
     monkey_true: usize,
     monkey_false: usize,
-    inspection_count: u32,
+    inspection_count: u64,
 }
 
 fn parse_item(input: &str) -> IResult<&str, Item> {
@@ -100,7 +100,7 @@ fn parse_starting_items_line(input: &str) -> IResult<&str, Vec<Item>> {
 }
 
 fn parse_operation_line(input: &str) -> IResult<&str, Operation> {
-    let (rest, (_, operand1, _, operator, _, operand2, _,)) = tuple((
+    let (rest, (_, operand1, _, operator, _, operand2, _)) = tuple((
         tag("  Operation: new = "),
         parse_operand,
         tag(" "),
@@ -121,11 +121,19 @@ fn parse_modulus_line(input: &str) -> IResult<&str, Item> {
 }
 
 fn parse_monkey_true_line(input: &str) -> IResult<&str, usize> {
-    delimited(tag("    If true: throw to monkey "), parse_usize, line_ending)(input)
+    delimited(
+        tag("    If true: throw to monkey "),
+        parse_usize,
+        line_ending,
+    )(input)
 }
 
 fn parse_monkey_false_line(input: &str) -> IResult<&str, usize> {
-    delimited(tag("    If false: throw to monkey "), parse_usize, line_ending)(input)
+    delimited(
+        tag("    If false: throw to monkey "),
+        parse_usize,
+        line_ending,
+    )(input)
 }
 
 fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
@@ -152,13 +160,17 @@ fn parse_monkeys(input: &str) -> IResult<&str, Vec<Monkey>> {
     separated_list1(line_ending, parse_monkey)(input)
 }
 
-fn play_turn(monkeys: Vec<Monkey>, thrower: usize) -> Vec<Monkey> {
+fn play_turn(monkeys: Vec<Monkey>, thrower: usize, modulus: Option<Item>) -> Vec<Monkey> {
     let mut new_monkeys = monkeys.clone();
     let m = &monkeys[thrower];
     let mut count = m.inspection_count;
     for item in m.items.iter() {
         let item = m.op.evaluate(*item);
-        let item = item / 3;
+        let item = if let Some(part2) = modulus {
+            item % part2
+        } else {
+            item / 3
+        };
         let catcher = if item % m.modulus == 0 {
             m.monkey_true
         } else {
@@ -172,25 +184,38 @@ fn play_turn(monkeys: Vec<Monkey>, thrower: usize) -> Vec<Monkey> {
     new_monkeys
 }
 
-fn play_round(mut monkeys: Vec<Monkey>) -> Vec<Monkey> {
+fn play_round(mut monkeys: Vec<Monkey>, modulus: Option<Item>) -> Vec<Monkey> {
     let l = monkeys.len();
     for i in 0..l {
-        monkeys = play_turn(monkeys, i);
+        monkeys = play_turn(monkeys, i, modulus);
     }
     monkeys
+}
+
+fn play_rounds(mut input: Vec<Monkey>, count: u64, modulus: Option<Item>) -> u64 {
+    for _ in 0..count {
+        input = play_round(input, modulus);
+    }
+    let mut counts: Vec<u64> = input.iter().map(|m| m.inspection_count).collect();
+    counts.sort();
+    counts.reverse();
+    counts[0] * counts[1]
 }
 
 fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().lock().read_to_string(&mut input).unwrap();
-    let (_, mut input) = parse_monkeys(&input[..]).unwrap();
+    let (_, input) = parse_monkeys(&input[..]).unwrap();
 
-    for round in 1..=20 {
-        input = play_round(input);
+    let part1 = play_rounds(input.clone(), 20, None);
+    println!("After the first 20 rounds: {:?}", part1);
+
+    let mut modulus = 1;
+    for m in input.iter() {
+        modulus *= m.modulus;
     }
-    let mut counts: Vec<u32> = input.iter().map(|m| m.inspection_count).collect();
-    counts.sort();
-    counts.reverse();
-    println!("{:?}", counts[..2].iter().product::<u32>());
+    let part2 = play_rounds(input.clone(), 10000, Some(modulus));
+    println!("Without worry decreasing after 1000 rounds: {:?}", part2);
+
     Ok(())
 }
