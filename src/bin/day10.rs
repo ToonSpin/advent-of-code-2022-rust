aa709aa0293cfd578d349abca4950227519660e1
Toon Spin <toon.spin@gmail.com>
2022-12-18 20:55:26 +0100

    Day 10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,13 +1,13 @@
 use std::io;
 use std::io::prelude::*;
 
-use nom::IResult;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, line_ending};
-use nom::combinator::{recognize, map_res, opt, value, map};
+use nom::combinator::{map, map_res, opt, recognize, value};
 use nom::multi::separated_list1;
 use nom::sequence::{pair, preceded};
+use nom::IResult;
 
 #[derive(Debug, Clone)]
 enum Instruction {
@@ -37,28 +37,46 @@ fn parse_instructions(input: &str) -> IResult<&str, Vec<Instruction>> {
     separated_list1(line_ending, parse_instruction)(input)
 }
 
-#[derive(Debug)]
-struct State {
+struct Tube {
+    instructions: Vec<Instruction>,
+    next_instruction: usize,
     x_register: i32,
-    elapsed_cycles: i32,
+    queue: Vec<i32>,
 }
 
-impl State {
-    fn new() -> Self {
-        State { x_register: 1, elapsed_cycles: 0 }
+impl Tube {
+    fn new(instructions: Vec<Instruction>) -> Self {
+        Tube {
+            instructions,
+            next_instruction: 0,
+            x_register: 1,
+            queue: Vec::new(),
+        }
     }
+}
 
-    fn iterate_part1(&mut self, instruction: &Instruction) -> i32 {
-        match instruction {
-            Instruction::Noop => {
-                self.elapsed_cycles += 1;
-                self.x_register
-            },
-            Instruction::Addx(i) => {
-                self.x_register += i;
-                self.elapsed_cycles += 2;
-                self.x_register - i
-            },
+impl Iterator for Tube {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.queue.pop() {
+            Some(val) => Some(val),
+            None => {
+                if self.instructions.len() <= self.next_instruction {
+                    return None;
+                }
+                let result = match self.instructions[self.next_instruction] {
+                    Instruction::Noop => Some(self.x_register),
+                    Instruction::Addx(argument) => {
+                        self.queue.push(self.x_register);
+                        self.queue.push(self.x_register);
+                        self.x_register += argument;
+                        self.queue.pop()
+                    }
+                };
+                self.next_instruction += 1;
+                result
+            }
         }
     }
 }
@@ -67,28 +85,31 @@ fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().lock().read_to_string(&mut input).unwrap();
     let (_, input) = parse_instructions(&input[..]).unwrap();
+    let tube = Tube::new(input.clone());
 
-    let mut state = State::new();
     let mut interesting_states = vec![220, 180, 140, 100, 60, 20];
     let mut total_signal_strength = 0;
+    let mut next_eval_part_one = interesting_states.pop().unwrap();
+    let mut crt_display = Vec::new();
 
-    let mut next_evaluation = interesting_states.pop().unwrap();
-    for instruction in input.iter() {
-        let prev_value = state.iterate_part1(instruction);
-        if state.elapsed_cycles >= next_evaluation {
-            total_signal_strength += next_evaluation * prev_value;
-            match interesting_states.pop() {
-                Some(n) => {
-                    next_evaluation = n
-                },
-                None => {
-                    break;
-                },
+    for (i, val) in tube.enumerate() {
+        let cycle = i as i32 + 1;
+        if cycle == next_eval_part_one {
+            total_signal_strength += next_eval_part_one * val;
+            if let Some(n) = interesting_states.pop() {
+                next_eval_part_one = n;
             }
         }
+        let column = (i as i32) % 40;
+        let pixel = if (val - column).abs() <= 1 { '#' } else { ' ' };
+        crt_display.push(pixel);
     }
+    println!("The total signal strength: {}", total_signal_strength);
 
-    println!("{}", total_signal_strength);
+    for line in crt_display.chunks(40) {
+        let line: String = line.iter().collect();
+        println!("{}", line);
+    }
 
     Ok(())
 }
